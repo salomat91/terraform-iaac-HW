2c9ca45f829b7b251accd8b569291a0b61cf80ff
salomat91 <hodjimatova@gmail.com>
2019-12-19 11:55:13 -0600

    changed bucket name

--- s3.tf
+++ s3.tf
@@ -3,7 +3,7 @@ resource "aws_s3_bucket" "b" {
   bucket = "my-tf-test-bucket" 
   acl    = "private" 
   tags = { 
-    Name        = "My bucket" 
+    Name        = "My bucket S" 
     Environment = "Dev" 
   } 
 } 
