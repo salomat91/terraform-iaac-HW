20248f2c7f33a2a4179590ef97713c5bffec6d61
salomat91 <hodjimatova@gmail.com>
2019-12-19 11:56:55 -0600

    changed name and region

--- s3.tf
+++ s3.tf
@@ -3,7 +3,7 @@ resource "aws_s3_bucket" "b" {
   bucket = "my-tf-test-bucket" 
   acl    = "private" 
   tags = { 
-    Name        = "My bucket S" 
+    Name        = "My bucket" 
     Environment = "Dev" 
   } 
 } 
