d01057e825378f8d5025c9b6079a23e66314fb7f
salomat91 <hodjimatova@gmail.com>
2019-12-23 16:48:47 -0600

    sg changed

--- sg.tf
+++ sg.tf
@@ -1,5 +1,5 @@
-resource "aws_security_group" "allow_ssh" { 
-  name        = "allow_ssh" 
+resource "aws_security_group" "allow_ssh_h" { 
+  name        = "allow_ssh_h" 
   description = "Allow ssh inbound traffic" 
   vpc_id      = "vpc-475dd123" 
   ingress { 
