97f650b1ad9fc827cf38327a96777b62ab3fbc99
salomat91 <hodjimatova@gmail.com>
2019-12-23 22:30:28 -0600

    variabled file was created

--- instance.tf
+++ instance.tf
@@ -1,18 +1,26 @@
 resource "aws_instance" "web" { 
-  ami           = "ami-00068cd7555f543d5" 
+  count = 10
+  ami           = var.ami 
   instance_type = "t2.micro" 
   associate_public_ip_address = "true" 
   key_name = aws_key_pair.deployer.key_name
   security_groups = ["allow_ssh"]
   user_data = file("userdata_file")
   
-  #lyfecycle{
-  #  prevent_destroy = True 
-  #}
+  lyfecycle{
+    prevent_destroy = True 
+  }
 
   tags = { 
 
-    Name = "HelloWorld"
+    Name = "HelloWorld${count.index +1}"
 
   } 
+}
+
+resource "aws_instance" "imported" {
+  ami = "ami-00068cd7555f543d5"
+  key_name = aws_key_pair.deployer.key_name 
+  security_groups = ["allow_ssh"]
+  instance_type - "t2.micro"
 }
