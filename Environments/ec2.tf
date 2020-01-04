421ce51453593302ab8f967805807d0ddf34ef3e
salomat91 <hodjimatova@gmail.com>
2020-01-04 13:39:09 -0600

    added tags

--- Environments/ec2.tf
+++ Environments/ec2.tf
@@ -2,4 +2,8 @@ resource "aws_instance" "instance" {
     ami              = var.ami
     instance_type    = var.instance_type
     key_name         = var.key_name
+    tags             = {
+
+        Name = var.environment
+    }
 }
