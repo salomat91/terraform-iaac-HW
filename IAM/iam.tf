8ecf2180d9844d9c3f5bf32ca4399af025474ece
salomat91 <hodjimatova@gmail.com>
2020-01-04 11:22:31 -0600

    << deleted

--- IAM/iam.tf
+++ IAM/iam.tf
@@ -24,7 +24,7 @@ resource "aws_iam_policy" "policy" {
   name = "test_policy3" 
   path = "/" 
   description = "My test policy" 
-  policy = <<EOF 
+  policy = EOF 
 { 
   "Version": "2012-10-17", 
   "Statement": [ 
