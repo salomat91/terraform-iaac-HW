0bbd07afea5131a5033774a4a4cf0ef803f68385
salomat91 <hodjimatova@gmail.com>
2020-01-04 10:23:47 -0600

    added developersteam

--- iam.tf
+++ iam.tf
@@ -11,7 +11,7 @@ resource "aws_iam_group" "developers" {
 }
 
 
-resource "aws_iam_group_membership" "developers" { 
+resource "aws_iam_group_membership" "developers_team" { 
   name = "developers-group-membership" 
   users = [ 
     aws_iam_user.tim.name 
