5ae85eb8292dc187ad90b91187fc3114db8d7f89
salomat91 <hodjimatova@gmail.com>
2020-01-04 11:32:44 -0600

    policy was added

--- IAM/iam.tf
+++ IAM/iam.tf
@@ -10,31 +10,14 @@ resource "aws_iam_group" "developers" {
     path ="/"
 }
 
-
-resource "aws_iam_group_membership" "developers_team" { 
-  name = "developers-group-membership" 
-  users = [ 
-    aws_iam_user.tim.name 
-  ] 
-  group = aws_iam_group.developers.name 
+data "aws_iam_policy_document" "example" { 
+  statement { 
+    actions = ["*"] 
+    resources = ["*"] 
+  } 
+} 
  
-} 
-
-resource "aws_iam_role" "test_role" { 
-name = "test_role" 
-assume_role_policy = <<EOF 
-{ 
-  "Version": "2012-10-17", 
-  "Statement": [ 
-{ 
-  "Action": "sts:AssumeRole", 
-  "Principal": { 
-  "Service": "ec2.amazonaws.com" 
-}, 
-  "Effect": "Allow", 
-  "Sid": "" 
-    } 
-  ] 
-} 
-EOF 
+resource "aws_iam_policy" "example" { 
+  name = "test_policy" 
+  policy = data.aws_iam_policy_document.example.json 
 } 
