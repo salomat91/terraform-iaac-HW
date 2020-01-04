45e4003c979905ff7d7f5286ff3f15ef4bea484f
salomat91 <hodjimatova@gmail.com>
2020-01-04 11:25:38 -0600

    resource was added

--- IAM/iam.tf
+++ IAM/iam.tf
@@ -24,17 +24,17 @@ resource "aws_iam_policy" "policy" {
   name = "test_policy3" 
   path = "/" 
   description = "My test policy" 
-  policy = EOF 
+  policy = <<EOF 
 { 
-  "Version": "2012-10-17", 
-  "Statement": [ 
-    { 
-      "Sid": "VisualEditor0", 
-      "Effect": "Allow", 
-      "Action": "ec2:*", 
-      "Resource": "*" 
-    } 
-  ] 
+"Version": "2012-10-17", 
+"Statement": [ 
+{ 
+"Sid": "VisualEditor0", 
+"Effect": "Allow", 
+"Action": "ec2:*", 
+"Resource": "*" 
+} 
+] 
 } 
 EOF 
 } 
