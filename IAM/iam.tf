9a69edebc7efdbb8e688ed5ffcd325a62453da05
salomat91 <hodjimatova@gmail.com>
2020-01-04 11:21:42 -0600

    spaces added

--- IAM/iam.tf
+++ IAM/iam.tf
@@ -26,15 +26,15 @@ resource "aws_iam_policy" "policy" {
   description = "My test policy" 
   policy = <<EOF 
 { 
-"Version": "2012-10-17", 
-"Statement": [ 
-{ 
-"Sid": "VisualEditor0", 
-"Effect": "Allow", 
-"Action": "ec2:*", 
-"Resource": "*" 
-} 
-] 
+  "Version": "2012-10-17", 
+  "Statement": [ 
+    { 
+      "Sid": "VisualEditor0", 
+      "Effect": "Allow", 
+      "Action": "ec2:*", 
+      "Resource": "*" 
+    } 
+  ] 
 } 
 EOF 
 } 
