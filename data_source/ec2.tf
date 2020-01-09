1670b0b90de43263b93e5e6a6df7703c9ff366d2
salomat91 <hodjimatova@gmail.com>
2020-01-08 19:09:59 -0600

    changed

--- data_source/ec2.tf
+++ data_source/ec2.tf
@@ -1,10 +1,10 @@
 provider "aws" {
-    region = "us-east-2" 
+    region = "us-east-1" 
 } 
 
 
 
-data "aws_ami" "latest" {
+data "aws_ami" "centos" {
     filter {
         name   = "name"
         values = ["CentOS Linux 7 x86_64 HVM EBS *"]
@@ -12,19 +12,19 @@ data "aws_ami" "latest" {
 
 
     most_recent = true 
-    owners = ["099720109477"] 
+    owners = ["679593333241"] 
 } 
 
 
 
 output "ami" {
-    value = "data.aws_ami.latest.id" 
+    value = data.aws_ami.centos.id
 } 
 
 
 
 resource "aws_instance" "web" {
-  ami           = "data.aws_ami.ubuntu.id"
+  ami           = data.aws_ami.centos.id
   instance_type = "t2.micro"
 
   tags = {
