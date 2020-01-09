b3aed18f0c677483360fd368397d670156b29f17
salomat91 <hodjimatova@gmail.com>
2020-01-08 19:04:00 -0600

    syntax changed

--- data_source/ec2.tf
+++ data_source/ec2.tf
@@ -18,13 +18,13 @@ data "aws_ami" "ubuntu" {
 
 
 output "ami" {
-    value = "${data.aws_ami.ubuntu.id}" 
+    value = "data.aws_ami.ubuntu.id" 
 } 
 
 
 
 resource "aws_instance" "web" {
-  ami           = "${data.aws_ami.ubuntu.id}"
+  ami           = "data.aws_ami.ubuntu.id"
   instance_type = "t2.micro"
 
   tags = {
