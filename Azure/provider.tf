942a936a1fce4cd2fdc49c0a9219181537bdefb7
salomat91 <hodjimatova@gmail.com>
2020-01-18 11:45:31 -0600

    provider added

--- Azure/provider.tf
+++ Azure/provider.tf
@@ -1,7 +1,11 @@
-provider "azurem" {
-    version = "1.27"
-}
-
+provider "azurerm" { 
+   version = "1.27" 
+} 
+ 
+ 
+ 
+ 
+ 
 resource "azurerm_resource_group" "web_server_rg" { 
   name = "web-rg" 
   location = "westus2" 
