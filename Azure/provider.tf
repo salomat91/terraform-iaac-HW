bf6bde8f4ed32d6fe112ecea5b09adb35e04c980
salomat91 <hodjimatova@gmail.com>
2020-01-18 11:50:19 -0600

    vnet added

--- Azure/provider.tf
+++ Azure/provider.tf
@@ -5,8 +5,20 @@ provider "azurerm" {
  
  
  
- 
 resource "azurerm_resource_group" "web_server_rg" { 
   name = "web-rg" 
   location = "westus2" 
 } 
+
+
+
+
+resource "azurerm_virtual_network" "vnet1" { 
+  name = "vnet1" 
+  location = "westus2" 
+  resource_group_name = "${azurerm_resource_group.web_server_rg.name}" 
+  address_space = ["10.0.0.0/16"] 
+} 
+ 
+
+
