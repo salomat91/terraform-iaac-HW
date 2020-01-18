2dfc9807338ab1a1dc71b647cd08ab7b4f7de2cd
salomat91 <hodjimatova@gmail.com>
2020-01-18 12:29:34 -0600

    second VM added

--- Azure/provider.tf
+++ Azure/provider.tf
@@ -112,4 +112,79 @@ resource "azurerm_virtual_machine" "vm1" {
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+resource "azurerm_network_interface" "nic2" { 
+  name = "nic2" 
+  location = "westus2" 
+  resource_group_name = "${azurerm_resource_group.web_server_rg.name}" 
+  ip_configuration { 
+    name = "testconfiguration2" 
+    subnet_id = "${azurerm_subnet.private.id}" 
+    private_ip_address_allocation = "Dynamic" 
+    public_ip_address_id = "${azurerm_public_ip.IP2.id}" 
+  } 
+} 
+ 
+ 
+resource "azurerm_public_ip" "IP2" { 
+  name = "public_ip2" 
+  location = "westus2" 
+  resource_group_name = "${azurerm_resource_group.web_server_rg.name}" 
+  allocation_method = "Dynamic" 
+} 
+ 
+ 
+ 
+resource "azurerm_virtual_machine" "vm2" { 
+  name = "vm2" 
+  location = "westus2" 
+  resource_group_name = "${azurerm_resource_group.web_server_rg.name}" 
+  network_interface_ids = ["${azurerm_network_interface.nic2.id}"] 
+  vm_size = "Standard_DS1_v2" 
+  storage_image_reference { 
+  publisher = "OpenLogic" 
+  offer = "CentOS" 
+  sku = "7.5" 
+  version = "latest" 
+} 
+storage_os_disk { 
+  name = "myosdisk2" 
+  caching = "ReadWrite" 
+  create_option = "FromImage" 
+  managed_disk_type = "Standard_LRS" 
+} 
+os_profile { 
+  computer_name = "vm2" 
+  admin_username = "centos" 
+} 
+os_profile_linux_config { 
+  disable_password_authentication = true 
+  ssh_keys { 
+  path = "/home/centos/.ssh/authorized_keys" 
+  key_data = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDDsv4s1yNrA7eWTyapRyuZ90amP7ZgSDX4JcUw23vbAZDY4CWjiAUO+xCwsZf8jvLJHZejirJGTpSQMwBJtHrT3O2B+ml8Hg+KJ+4o5yWuegTGSWO745hbdD960ezYLGK9XPvs8uNzbGKbzQuWxDu5v5I+J+uBO+ksrHt0Zzcz3bTtfkJlBgnOHAs3CC8bZZFVy1GUuTQwaqhCQBNOagjVSGVZ0V410aK99uorZxxYZac5EKBmbeWv5sI395p5Awa2F6TLuIGyrCI01yTg+j/3l5sJwd8F/Kn451+9FKhlB17Y17IPq7WQ8X5BNcu/Z8z4JVYa2KY1ZJ2E1jblYp+j root@ip-172-31-19-54.ec2.internal"
+  } 
+} 
+tags { 
+   environment = "staging" 
+  } 
+} 
+
  
